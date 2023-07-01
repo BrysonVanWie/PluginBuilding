f94021bab427ea923714a099bb7198487aa6745d
= <--replace-all>
2023-07-01 13:00:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PluginBuilding
+
+Developed with Unreal Engine 5
---
+++ Source/PluginBuilding/PluginBuildingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginBuildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINBUILDING_API APluginBuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
